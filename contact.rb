3b19f62e29346bf4ad7bbc4b0fc35efd38605f51
Amanda Punsammy <amanda.punsammy@gmail.com>
2017-04-03 20:44:12 -0400

    contact method self.find

--- contact.rb
+++ contact.rb
@@ -1,32 +1,44 @@
 class Contact
 
+  attr_reader :id
+  attr_accessor :first_name, :last_name, :email, :note
+
+  @@contacts = []
+  @@id = 1
   # This method should initialize the contact's attributes
-  def initialize
-
+  def initialize(first_name, last_name, email, note)
+    @first_name = first_name
+    @last_name = last_name
+    @email = email
+    @note = note
+    @id = @@id
+    @@id += 1
   end
 
-  # This method should call the initializer, 
+  # This method should call the initializer,
   # store the newly created contact, and then return it
-  def self.create
-
+  def self.create(first_name, last_name, email, note)
+    new_contact = Contact.new(first_name, last_name, email, note)
+    @@contacts << new_contact
+    return new_contact
   end
 
-  # This method should return all of the existing contacts
-  def self.all
-
+  def self.all       # This method should return all of the existing contacts
+    @@contacts
   end
 
-  # This method should accept an id as an argument
-  # and return the contact who has that id
-  def self.find
-
+  def self.find(id)                 # This method should accept an id as an argument
+    @@contacts.find do |contact|
+      contact.id == id              # and return the contact who has that id
+    end
   end
 
-  # This method should allow you to specify 
+  # This method should allow you to specify
   # 1. which of the contact's attributes you want to update
   # 2. the new value for that attribute
   # and then make the appropriate change to the contact
   def update
+    
 
   end
 
@@ -44,7 +56,7 @@ class Contact
   end
 
   def full_name
-
+    "#{first_name} #{last_name}"
   end
 
   # This method should delete the contact
@@ -54,5 +66,5 @@ class Contact
   end
 
   # Feel free to add other methods here, if you need them.
-  
+
 end
